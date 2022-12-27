5957a956ddc6635876aea21ea4c72127fb1a0f73
ShubhamMauryaRk <1234shubhamlalita@gmail.com>
2022-12-27 15:45:17 +0530

    Final commit

--- client.js
+++ client.js
@@ -1,10 +1,2 @@
-const socket=io('http://localhost:8000');
+const socket=io();
 
-const form=document.querySelector('.msgBox form');
-const formInput=document.querySelector('.msgBox form input');
-const formBtn=document.querySelector('.msgBox form button');
-const container=document.querySelector('.container');
-
-const name=prompt("Enter your name: ");
-socket.emit('new-user-joined',name);
-console.log(name)
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,15 +1,31 @@
-const io=require('socket.io')(8000);
-const users={};
+const express = require('express');
+const app = express();
 
-io.on('connection',(socket)=>{
-    socket.on('new-user-joined',name=>{
-        console.log(name);
-        users[socket.id]=name;
-        socket.broadcast.emit('user-joined',name);
-    });
+const http = require('http');
+const server = http.createServer(app);
+const users = {};
 
-    socket.on('send',msg=>{
-        socket.broadcast.emit('receive',{message:msg,name:user[socket.id]});
-    });
-
+app.use(express.static("C:\\Shubham\\Shubham Projects\\Whatsapp clone\\public"));
+app.get('/', (req, res) => {
+    res.sendFile('C:\\Shubham\\Shubham Projects\\Whatsapp clone\\index.html');
 });
+
+//socket.io setup
+
+const io = require('socket.io')(server);
+io.on("connection",(socket)=>{
+    socket.on("new-user-joined",name=>{
+        users[socket.id]=name;
+        console.log(users);
+        socket.broadcast.emit("user-joined",name);
+    })
+    socket.on('send',(data)=>{
+        socket.broadcast.emit("message",{"username":users[socket.id],"msg":data["msg"]});
+    })
+})
+
+//socket.io setup ends
+
+server.listen(3000,()=>{
+    console.log("Server connected at: 3000");
+})
